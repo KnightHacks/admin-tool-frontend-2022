ed8cb70d7822d9205e101238b6122c456273c3fa
conroyr41 <conroyr41@gmail.com>
2022-08-14 16:59:28 -0400

    Home Page
    Added some basic stuff to get started like the outline of the main toolbar, the header text, a subheader, and the outline of a button.

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,19 @@
+import './home.css';
 import React from "react"
 
 export default function Home(){
-    return (<div><h1>HOME</h1></div>);
+    var userName = "John"
+
+    return (
+        <div>
+            <div class="main-toolbar"/>
+
+            <h1 class="header-text">Welcome, {userName}.</h1>
+            <button class="hackathon-analytics-button">
+                <body class="button-text">Hackathon Analytics</body>
+            </button>
+
+            <h1 class="subheader-text">Upcoming Hackathons</h1>
+        </div>
+    )
 }
