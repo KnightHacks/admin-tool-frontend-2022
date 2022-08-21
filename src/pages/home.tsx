84d84d44040a5c769808b03a302b7fbb288a127d
Jadyn LaPace <jadynlapace@gmail.com>
2022-08-21 14:41:10 -0400

    Added some basic elements for the Home Page.

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -8,11 +8,17 @@ export default function Home() {
         <div className="main-toolbar"/>
 
         <h1 className="header-text">Welcome, {userName}.</h1>
-        <button className="hackathon-analytics-button">
-            <body className="button-text">Hackathon Analytics</body>
-        </button>
+        <button className="hackathon-analytics-button"/>
+          
+        <body className="hackathon-analytics-text">Hackathon Analytics</body>
 
-        <h1 className="subheader-text">Upcoming Hackathons</h1>
+        <h1 className="subheader-text-1">Upcoming Hackathons</h1>
+
+        <button className="create-hackathon-button"/>
+        <body className="create-hackathon-text">Create Hackathon</body>
+
+
+        <h1 className="subheader-text-2">Recent Hackathons</h1>
     </div>
   );
 }
