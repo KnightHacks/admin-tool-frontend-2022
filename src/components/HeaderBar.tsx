42123348dbe2472dad2b20b0852863c770ea5490
ElijahMSmith <mcditto99@gmail.com>
2022-08-22 11:20:55 -0400

    Button -> Link

--- src/components/HeaderBar.tsx
+++ src/components/HeaderBar.tsx
@@ -1,29 +1,25 @@
 import { ReactComponent as KHLogo } from '../assets/logo.svg';
 import { ReactComponent as DrawerHamburger } from '../assets/drawerHamburger.svg';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function HeaderBar() {
 	let navigate = useNavigate();
 
 	return (
-		<div className="flex justify-between w-full p-4 shadow-headerbar mb-3">
+		<div className="flex justify-between w-full p-8 shadow-headerbar mb-3">
 			<div className="flex items-center">
-				<button
-					onClick={() => {
-						/*TODO*/
-					}}
-				>
+				{/* TODO: Open drawer */}
+				<Link to="/">
 					<DrawerHamburger className="mr-4" />
-				</button>
-
-				<button onClick={() => navigate('/')}>
+				</Link>
+				<Link to="/">
 					<KHLogo />
-				</button>
+				</Link>
 			</div>
 
-			<button onClick={() => navigate('/settings')}>
+			<Link to="/settings">
 				<div className="bg-orange-600 w-8 h-8 rounded-full" />
-			</button>
+			</Link>
 		</div>
 	);
 }
