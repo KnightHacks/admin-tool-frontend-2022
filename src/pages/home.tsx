64170edc29a53dc8b6aec6ecc267b9f5ff7e24eb
conroyr41 <conroyr41@gmail.com>
2022-08-23 12:49:27 -0400

    Added some TODO comments and refactored the home page.

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,21 +4,29 @@ import React from "react";
 export default function Home() {
   var userName = "John"
   return (
-    <div>
+    <div>      
+        {/* TODO: Add Main Toolbar Component here */}
         <div className="main-toolbar"/>
 
         <h1 className="header-text">Welcome, {userName}.</h1>
-        <button className="hackathon-analytics-button"/>
-          
-        <body className="hackathon-analytics-text">Hackathon Analytics</body>
+
+        <div>
+          <button className="hackathon-analytics-button"/>          
+          <body className="hackathon-analytics-text">Hackathon Analytics</body>
+        </div>
 
         <h1 className="subheader-text-1">Upcoming Hackathons</h1>
+        
+        {/* TODO: Add Button Page Links Here */}
 
-        <button className="create-hackathon-button"/>
-        <body className="create-hackathon-text">Create Hackathon</body>
-
+        <div>
+          <button className="create-hackathon-button"/>
+          <body className="create-hackathon-text">Create Hackathon</body>
+        </div>
 
         <h1 className="subheader-text-2">Recent Hackathons</h1>
+        
+        {/* TODO: Add Table Component Here */}
     </div>
   );
 }
