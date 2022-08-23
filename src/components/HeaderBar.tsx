ff2005e6b679373b28856f52dd0ca2ff57aa1c93
Eli <mcditto99@gmail.com>
2022-08-22 20:13:43 -0400

    Fix padding

--- src/components/HeaderBar.tsx
+++ src/components/HeaderBar.tsx
@@ -3,9 +3,8 @@ import { ReactComponent as DrawerHamburger } from '../assets/drawerHamburger.svg
 import { Link } from 'react-router-dom';
 
 export default function HeaderBar() {
-
 	return (
-		<div className="flex justify-between w-full p-8 shadow-headerbar mb-3">
+		<div className="flex justify-between w-full p-4 shadow-headerbar mb-3">
 			<div className="flex items-center">
 				{/* TODO: Open drawer */}
 				<Link to="/">
