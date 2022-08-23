78a4e476cb73878b1eff74ff76497fd4a878d7e9
conroyr41 <conroyr41@gmail.com>
2022-08-23 15:54:39 -0400

    Added a few icons.

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,32 +1,34 @@
 import './home.css';
 import React from "react";
+import { ReactComponent as CreateHackathonIcon } from '../assets/createHackathonIcon.svg';
+import { ReactComponent as HackathonAnalyticsIcon } from '../assets/hackathonAnalyticsIcon.svg';
 
 export default function Home() {
   var userName = "John"
   return (
-    <div>      
-        {/* TODO: Add Main Toolbar Component here */}
-        <div className="main-toolbar"/>
-
+    <div>
+      <div>
+        <div className="generic-profile-icon"/>
         <h1 className="header-text">Welcome, {userName}.</h1>
+      </div>
 
-        <div>
-          <button className="hackathon-analytics-button"/>          
-          <body className="hackathon-analytics-text">Hackathon Analytics</body>
-        </div>
+      <button className="hackathon-analytics-button">          
+        <HackathonAnalyticsIcon className="hackathon-analytics-icon"/>
+      </button>
+      <body className="hackathon-analytics-text">Hackathon Analytics</body>
 
-        <h1 className="subheader-text-1">Upcoming Hackathons</h1>
-        
-        {/* TODO: Add Button Page Links Here */}
+      <h1 className="subheader-text-1">Upcoming Hackathons</h1>
 
-        <div>
-          <button className="create-hackathon-button"/>
-          <body className="create-hackathon-text">Create Hackathon</body>
-        </div>
+      {/* TODO: Add Button Page Links Here */}
 
-        <h1 className="subheader-text-2">Recent Hackathons</h1>
-        
-        {/* TODO: Add Table Component Here */}
+      <button className="create-hackathon-button">          
+        <CreateHackathonIcon className="create-hackathon-icon"/>
+      </button>
+      <body className="create-hackathon-text">Create Hackathon</body>
+
+      <h1 className="subheader-text-2">Recent Hackathons</h1>
+
+      {/* TODO: Add Table Component Here */}
     </div>
   );
 }
