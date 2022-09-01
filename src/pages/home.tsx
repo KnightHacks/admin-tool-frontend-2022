a6312c23365fe804bc788e74d992cab6fa218a5a
conroyr41 <conroyr41@gmail.com>
2022-09-01 17:29:27 -0400

    Reduced repetitive code.

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,4 @@
 import './home.css';
-import React from 'react';
 import { ReactComponent as PageLinksIcon } from '../assets/arrow.svg';
 import { ReactComponent as CreateHackathonIcon } from '../assets/createHackathonIcon.svg';
 import { ReactComponent as HackathonAnalyticsIcon } from '../assets/hackathonAnalyticsIcon.svg';
@@ -8,6 +7,38 @@ import { Link } from 'react-router-dom';
 export default function Home() {
 	// TODO: This user name should be generic.
 	let userName: string = 'John';
+
+	const pageLinks = [
+		{
+			route: '/hackathon',
+			buttonText: 'Go to Hackathon',
+		},
+
+		{
+			route: '/attendees',
+			buttonText: 'Attendees',
+		},
+
+		{
+			route: '/sponsors',
+			buttonText: 'Sponsors',
+		},
+
+		{
+			route: '/edit-details',
+			buttonText: 'Edit Details',
+		},
+
+		{
+			route: '/schedule',
+			buttonText: 'Schedule',
+		},
+
+		{
+			route: '/settings',
+			buttonText: 'Settings',
+		},
+	];
 
 	return (
 		<div id="page">
@@ -33,59 +64,16 @@ export default function Home() {
 				<h1 id="page-links-header">Fall 2022</h1>
 
 				<div id="page-links-grid">
-					<Link to="/hackathon">
-						<button className="page-links-grid-button">
-							<PageLinksIcon className="page-links-grid-button-icon" />
-							<body className="page-links-grid-button-text">
-								Go to Hackathon
-							</body>
-						</button>
-					</Link>
-
-					<Link to="/attendees">
-						<button className="page-links-grid-button">
-							<PageLinksIcon className="page-links-grid-button-icon" />
-							<body className="page-links-grid-button-text">
-								Attendees
-							</body>
-						</button>
-					</Link>
-
-					<Link to="/sponsors">
-						<button className="page-links-grid-button">
-							<PageLinksIcon className="page-links-grid-button-icon" />
-							<body className="page-links-grid-button-text">
-								Sponsors
-							</body>
-						</button>
-					</Link>
-
-					<Link to="/edit-details">
-						<button className="page-links-grid-button">
-							<PageLinksIcon className="page-links-grid-button-icon" />
-							<body className="page-links-grid-button-text">
-								Edit Details
-							</body>
-						</button>
-					</Link>
-
-					<Link to="/schedule">
-						<button className="page-links-grid-button">
-							<PageLinksIcon className="page-links-grid-button-icon" />
-							<body className="page-links-grid-button-text">
-								Schedule
-							</body>
-						</button>
-					</Link>
-
-					<Link to="/settings">
-						<button className="page-links-grid-button">
-							<PageLinksIcon className="page-links-grid-button-icon" />
-							<body className="page-links-grid-button-text">
-								Settings
-							</body>
-						</button>
-					</Link>
+					{pageLinks.map((pageLink) => (
+						<Link to={pageLink.route}>
+							<button className="page-links-grid-button">
+								<PageLinksIcon className="page-links-grid-button-icon" />
+								<body className="page-links-grid-button-text">
+									{pageLink.buttonText}
+								</body>
+							</button>
+						</Link>
+					))}
 				</div>
 			</div>
 
