151cc660d8c4624f32f03de80271890e8e704754
Victoria Williamson <vicwill01@knights.ucf.edu>
2022-09-05 02:52:12 -0400

    adding multi day events

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -5,13 +5,14 @@ import './styles.css';
  * @param headerContent contains component for an optional header content. If there are going to be column titles, each needs to be in a <th> </th> tag.
  * @param bodyContent contains all of the rows that will be inside the table.
  */
+
 export default function Table(props: {
 	headerContent?: React.ReactElement;
 	bodyContent?: React.ReactElement;
 }) {
 	return (
 		<>
-			<div className="h-full w-full overflow-auto  p-5">
+			<div className="h-full w-full overflow-auto p-5">
 				<table className="w-full border-collapse border-spacing-0 p-5">
 					<tbody>
 						<tr> {props.headerContent} </tr>
