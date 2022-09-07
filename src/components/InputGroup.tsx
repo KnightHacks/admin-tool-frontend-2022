90e700bf898bf28a3f16aed018a6d52b7aa02b26
Victoria Williamson <vicwill01@knights.ucf.edu>
2022-09-07 11:06:13 -0400

    Fixing input group

--- src/components/InputGroup.tsx
+++ src/components/InputGroup.tsx
@@ -5,7 +5,7 @@ import { HTMLInputTypeAttribute } from 'react';
  * @param value initial value for the input.
  * @param setValue function that takes the updated inputValue and sets it.
  * @param label label for the input.
- * @param type type of input
+ * @param type type of input, by default this is text
  */
 export default function InputGroup(props: {
 	value: any;
@@ -20,7 +20,7 @@ export default function InputGroup(props: {
 			</div>
 			<input
 				className="w-3/4 max-w-[350px] p-2 bg-popup-input-bg border-solid border-popup-input-border border-[1px] rounded-lg shadow-sm bored-r-8 "
-				type={props.type}
+				type={props.type ?? 'text'}
 				value={props.value}
 				onChange={(e) => {
 					props.setValue(e.target.value);
