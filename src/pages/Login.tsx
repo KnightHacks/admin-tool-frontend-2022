ae7f80e7e299b855d68418aa122fe5e0584ab321
conroyr41 <conroyr41@gmail.com>
2022-10-10 23:11:31 -0400

    Added the buttons and finished most of the styling.

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,8 @@
 import React from "react"
 import '../components/login/Login.css'
 import { ReactComponent as KHLogo } from '../assets/logo.svg'
+import { ReactComponent as GoogleLogo } from '../assets/login/google.svg'
+import { ReactComponent as GitHubLogo } from '../assets/login/github.svg'
 
 export default function Login() {
     return (
@@ -9,9 +11,21 @@ export default function Login() {
                 <KHLogo id='page-header-icon'/>
                 <h1 id='page-header-text'>Admin Tool</h1>
             </div>
-            <div className='break' />
-            <div id='login-contianer'>
-                
+            <div id='login-container'>
+                <h1 id='login-header-text'>Sign In</h1>
+                <button id='github-button'>
+                    <p id='github-button-text'>Sign in with GitHub</p>
+                    <GitHubLogo id='github-button-icon'/>
+                </button>
+                <div id='or-container'>
+                    <div className='or-seperator'/>
+                    <p>OR</p>
+                    <div className='or-seperator'/>
+                </div>
+                <button id='google-button'>
+                    <p>Sign in with </p>
+                    <GoogleLogo id='google-button-icon'/>
+                </button>
             </div>
         </div>
     )
