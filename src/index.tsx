986bcc46505e4c661d225a20d14914cbe6908e76
Victoria Williamson <vicwill01@knights.ucf.edu>
2022-10-13 12:08:59 -0400

    add graphql

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+	ApolloClient,
+	InMemoryCache,
+	ApolloProvider,
+	gql,
+} from '@apollo/client';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+	document.getElementById('root') as HTMLElement
 );
+
+console.log(process.env.PUBLIC_URL);
+const client = new ApolloClient({
+	uri: process.env.REACT_APP_URI,
+	cache: new InMemoryCache(),
+});
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<ApolloProvider client={client}>
+		<App />
+	</ApolloProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
