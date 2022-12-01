10ceb575e81a4ba77658d22b70f72328a561e9d4
Elijah Smith <mcditto99@gmail.com>
2022-12-01 16:02:17 -0500

    Get amount function

--- src/models/sponsor.ts
+++ src/models/sponsor.ts
@@ -16,14 +16,26 @@ export enum HackathonTier {
 	SILVER = 'Silver',
 	GOLD = 'Gold',
 	PLATINUM = 'Platinum',
+	CUSTOM = 'Custom',
 }
 
-export const allTiers = ['Bronze', 'Silver', 'Gold', 'Platinum']
+export const allTiers = ['Bronze', 'Silver', 'Gold', 'Platinum', 'Custom']
 
 export function isValidTier(val: string) {
 	return allTiers.indexOf(val) !== -1
 }
 
 export function getAmount(tier: string) {
-	return 0 // TODO
+	switch (tier) {
+		case 'Bronze':
+			return 3000
+		case 'Silver':
+			return 5000
+		case 'Gold':
+			return 7000
+		case 'Platinum':
+			return 10000
+		default:
+			return 0
+	}
 }
