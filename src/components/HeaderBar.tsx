40c5b9b013fb2e3a8097d65d1ca2bd86485abba9
Elijah Smith <mcditto99@gmail.com>
2023-01-01 17:29:14 -0500

    Replace profile icon with WIP menu

--- src/components/HeaderBar.tsx
+++ src/components/HeaderBar.tsx
@@ -1,57 +1,112 @@
 import { ReactComponent as KHLogo } from '../assets/logo.svg'
-import { ReactComponent as ProfileIcon } from '../assets/profile.svg'
 import { Link } from 'react-router-dom'
+import { Menu, Transition } from '@headlessui/react'
+import { Fragment } from 'react'
+import { UserCircleIcon } from '@heroicons/react/20/solid'
 
 export default function HeaderBar() {
-	const openProfileSettings = () => {
-		// TODO: Open a popover menu with links to do various things
-		// Only link for now is just to log out, but could add more in future years
-		//  {
-		// 		;(localStorage.getItem('code') ||
-		// 			localStorage.getItem('state')) && (
-		// 			<div
-		// 				onClick={() => {
-		// 					localStorage.removeItem('code')
-		// 					localStorage.removeItem('state')
-		// 					localStorage.removeItem('accessToken')
-		// 					localStorage.removeItem('refreshToken')
-		// 					window.location.reload()
-		// 				}}
-		// 			>
-		// 				<Link to="/auth">
-		// 					<button className="bg-eraser px-6 py-2 rounded-lg font-bold skew-y-0 hover:skew-y-2 eraserOne">
-		// 						Sign Out
-		// 					</button>
-		// 				</Link>
-		// 			</div>
-		// 		)
-		// 	}
-		// 	{
-		// 		!(
-		// 			localStorage.getItem('code') ||
-		// 			localStorage.getItem('state')
-		// 		) && (
-		// 			<Link to="/register">
-		// 				<button className="bg-eraser px-6 py-2 rounded-lg font-bold skew-y-0 hover:skew-y-2 eraserOne">
-		// 					Register Now
-		// 				</button>
-		// 			</Link>
-		// 		)
-		// 	}
-	}
-
 	return (
 		<div className="flex mb-2 justify-between w-full p-4 shadow-headerbar">
 			<Link to="/">
 				<KHLogo />
 			</Link>
-			{/* TODO: Change this button into a settings icon or some kind of menu icon */}
-			<button
-				className="w-8 h-8 rounded-full"
-				onClick={openProfileSettings}
-			>
-				<ProfileIcon />
-			</button>
+			{/* TODO: Put this in top right corner and fix all the stylings */}
+			<div className="fixed top-16 w-56 text-right">
+				<Menu as="div" className="relative inline-block text-left">
+					<div>
+						<Menu.Button className="inline-flex w-full justify-center rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+							Options
+							<UserCircleIcon
+								className="ml-2 -mr-1 h-5 w-5 text-violet-200"
+								aria-hidden="true"
+							/>
+						</Menu.Button>
+					</div>
+					<Transition
+						as={Fragment}
+						enter="transition ease-out duration-100"
+						enterFrom="transform opacity-0 scale-95"
+						enterTo="transform opacity-100 scale-100"
+						leave="transition ease-in duration-75"
+						leaveFrom="transform opacity-100 scale-100"
+						leaveTo="transform opacity-0 scale-95"
+					>
+						<Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+							<div className="px-1 py-1 ">
+								<Menu.Item>
+									{({ active }) => (
+										<button
+											className={`${
+												active
+													? 'bg-violet-500 text-white'
+													: 'text-gray-900'
+											} group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+										>
+											Edit
+										</button>
+									)}
+								</Menu.Item>
+								<Menu.Item>
+									{({ active }) => (
+										<button
+											className={`${
+												active
+													? 'bg-violet-500 text-white'
+													: 'text-gray-900'
+											} group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+										>
+											Duplicate
+										</button>
+									)}
+								</Menu.Item>
+							</div>
+							<div className="px-1 py-1">
+								<Menu.Item>
+									{({ active }) => (
+										<button
+											className={`${
+												active
+													? 'bg-violet-500 text-white'
+													: 'text-gray-900'
+											} group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+										>
+											Archive
+										</button>
+									)}
+								</Menu.Item>
+								<Menu.Item>
+									{({ active }) => (
+										<button
+											className={`${
+												active
+													? 'bg-violet-500 text-white'
+													: 'text-gray-900'
+											} group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+										>
+											Move
+										</button>
+									)}
+								</Menu.Item>
+							</div>
+							<div className="px-1 py-1">
+								<Menu.Item>
+									{({ active }) => (
+										<button
+											className={`${
+												active
+													? 'bg-violet-500 text-white'
+													: 'text-gray-900'
+											} group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+										>
+											Delete
+										</button>
+									)}
+								</Menu.Item>
+							</div>
+						</Menu.Items>
+					</Transition>
+				</Menu>
+			</div>
 		</div>
 	)
 }
